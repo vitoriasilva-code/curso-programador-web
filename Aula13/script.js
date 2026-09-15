8b993657b9d3fa77684d32de1eb87e4a18ab6715
Neuza Vitoria <neuzav028@gmail.com>
2026-09-15 11:22:56 -0300

    javascript

---
+++ Aula13/script.js
@@ -0,0 +1,10 @@
+// let valor = Number(prompt("Digite um valor:"));
+// let desconto = valor * 0.15
+// let resultado = valor - desconto;
+
+// console.log("Valor final com desconto" + resultado);
+
+let num1 = Number(prompt("Digite o primerio numero: "));
+let num2 = Number(prompt("Digite o segundo numero: "));
+let divisao = num1 / num2
+ alert("O resultado da divisão é" + divisao)
